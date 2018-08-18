17306d57626e69d76899e6dec3cf6b1e8624418d
speedwellbot <42114274+speedwellbot@users.noreply.github.com>
2018-08-18 23:40:02 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 exports.run = function(client, message, args) {
 
   if (!message.guild) {
-    return message.author.send('`temizle,sil` komutları sadece sunucularda kullanılabilir Veled Ben Salakmıyım.');
+    return message.author.send('`temizle,sil` komutları sadece sunucularda Kullanılabilir !!');
   }
   let mesajsayisi = parseInt(args.join(' '));
   if (mesajsayisi.length < 0) return message.channel.send('Kaç mesaj silmem gerektiğini belirtmedin Knk :smile:.')
-  if (mesajsayisi > 100) return message.channel.send('100 adetden fazla mesaj silemem Aq Okadar da Deil!');
+  if (mesajsayisi > 100) return message.channel.send('99 Mesajdan Fazlasını Silemem :fire: !');
   message.channel.bulkDelete(mesajsayisi + 2);
   message.channel.send(mesajsayisi +' Adet Mesajınız Silindi :fire: !')
   message.channel.bulkDelete(mesajsayisi + 1);
